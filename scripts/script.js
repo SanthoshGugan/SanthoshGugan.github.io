e36a72e231a6923eccdc0d96895a715cec8ec989
Santhosh Gugan <santhoshg1990@gmail.com>
2017-10-06 23:50:02 +0530

    #about #header scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,21 @@
 function init(){
+	var lastDistanceY = 0;
 	window.addEventListener('scroll', function(){
-		var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+		var distanceY = window.pageYOffset || document.documentElement.scrollTop
 		var shrinkOn = 50;
 		var header = document.getElementById("header");
-		if(distanceY > shrinkOn){
+		if(lastDistanceY > distanceY && distanceY > shrinkOn){
+			header.classList.add('whiteVanish');
+			header.classList.remove('vanish');
+		}else if(distanceY > shrinkOn){
+			header.classList.remove('whiteVanish');
 			header.classList.add('vanish');
 		}else{
 			header.classList.remove('vanish');
+			header.classList.remove('whiteVanish');
 		}
+		;
+		lastDistanceY =  distanceY;
 
 	});
 }
