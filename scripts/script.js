0e8c2aefa55c632acfae0c2cf3d86a66f774c03d
Santhosh Gugan <santhoshg1990@gmail.com>
2017-10-06 23:25:30 +0530

    scroll down

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 function init(){
 	window.addEventListener('scroll', function(){
 		var distanceY = window.pageYOffset || document.documentElement.scrollTop;
-		var shrinkOn = 300;
+		var shrinkOn = 50;
 		var header = document.getElementById("header");
 		if(distanceY > shrinkOn){
 			header.classList.add('vanish');
-
+		}else{
+			header.classList.remove('vanish');
 		}
 
 	});
