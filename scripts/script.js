be0cce89e43e0f32744e9820e336acc13e8caccc
Santhosh Gugan <santhoshg1990@gmail.com>
2017-10-10 23:45:55 +0530

    sprint icons

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,26 @@
+var count = 0;
+var timeoutID;
+var intervalID;
+function removeClasses(sprintElement){
+	if(!!sprintElement && sprintElement.classList.contains('circleNext'))
+			sprintElement.classList.remove('circleNext');
+	if(!!sprintElement && sprintElement.classList.contains('circleAnother'))
+			sprintElement.classList.remove('circleAnother');
+}
+function changeSprintPosition(){
+	var sprintElement = document.getElementById('circleSprint');
+	removeClasses(sprintElement);
+	 if(count == 1 && !!sprintElement){
+		sprintElement.classList.add('circleNext');
+	}else if(count == 2 && !!sprintElement){
+		sprintElement.classList.add('circleAnother');
+	}else if(count == 3 && !!sprintElement){
+		sprintElement.classList.add('circleNext');
+	}
+	count = (count + 1) % 4;
+	
+
+};
 function init(){
 	var lastDistanceY = 0;
 	window.addEventListener('scroll', function(){
@@ -17,6 +40,8 @@ function init(){
 		lastDistanceY =  distanceY;
 
 	});
+
+	intervalID = window.setInterval(changeSprintPosition,3000);
 }
 
 window.onload = init();
