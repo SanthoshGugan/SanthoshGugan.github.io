de5200de311f75589e981b248de1d11d996ba8c8
Santhosh Gugan <santhoshg1990@gmail.com>
2017-10-12 01:14:43 +0530

    Develop what you need

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,39 @@
-var count = 0;
-var timeoutID;
-var intervalID;
+var count1 = 0;
+var count2= 0;
+var intervalID1;
+var intervalID2;
 function removeClasses(sprintElement){
 	if(!!sprintElement && sprintElement.classList.contains('circleNext'))
 			sprintElement.classList.remove('circleNext');
 	if(!!sprintElement && sprintElement.classList.contains('circleAnother'))
 			sprintElement.classList.remove('circleAnother');
 }
-function changeSprintPosition(){
-	var sprintElement = document.getElementById('circleSprint');
-	removeClasses(sprintElement);
-	 if(count == 1 && !!sprintElement){
-		sprintElement.classList.add('circleNext');
-	}else if(count == 2 && !!sprintElement){
-		sprintElement.classList.add('circleAnother');
-	}else if(count == 3 && !!sprintElement){
-		sprintElement.classList.add('circleNext');
+function changeSprint1Position(){
+	var sprint1Element = document.getElementById('circleSprint1');
+	removeClasses(sprint1Element);
+	 if(count1 == 1 && !!sprint1Element){
+		sprint1Element.classList.add('circleNext');
+	}else if(count1 == 2 && !!sprint1Element){
+		sprint1Element.classList.add('circleAnother');
+	}else if(count1 == 3 && !!sprint1Element){
+		sprint1Element.classList.add('circleNext');
 	}
-	count = (count + 1) % 4;
+	count1 = (count1 + 1) % 4;
+	
+
+};
+
+function changeSprint2Position(){
+	var sprint2Element = document.getElementById('circleSprint2');
+	removeClasses(sprint2Element);
+	 if(count2 == 1 && !!sprint2Element){
+		sprint2Element.classList.add('circleNext');
+	}else if(count2 == 2 && !!sprint2Element){
+		sprint2Element.classList.add('circleAnother');
+	}else if(count2 == 3 && !!sprint2Element){
+		sprint2Element.classList.add('circleNext');
+	}
+	count2 = (count2 + 1) % 4;
 	
 
 };
@@ -41,7 +57,8 @@ function init(){
 
 	});
 
-	intervalID = window.setInterval(changeSprintPosition,3000);
+	intervalID1 = window.setInterval(changeSprint1Position,3000);
+	intervalID2 = window.setInterval(changeSprint2Position,3000);
 }
 
 window.onload = init();
